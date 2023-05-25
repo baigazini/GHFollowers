abc5dbda47fb1a241bfd844a5a99932e619da6fa
Alisher Baigazin <alisherbaigazin@gmail.com>
2023-05-25 19:13:16 +0600

    initial project setup

---
+++ GHFollowers/GHFollowers/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  GHFollowers
+//
+//  Created by Alisher Baigazin on 25.05.2023.
+//
+
+import UIKit
+
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
